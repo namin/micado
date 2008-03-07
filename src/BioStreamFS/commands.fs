d1e64de0dcb64824715bfcb500db6d5b5be480ae
Nada AMIN <Nada AMIN@b571c760-3347-0410-a02b-4f11275fb890>
2008-03-07 05:00:47 +0000

    Added official ConnectValvesToPunches command.

--- src/BioStreamFS/commands.fs
+++ src/BioStreamFS/commands.fs
@@ -28,4 +28,39 @@ let placeValve() =
  |> Option.map (fun (entity) -> entity :?> Valve)
  |> Option.map (fun (valve) -> Editor.writeLine ("Created valve at" ^ valve.Center.ToString() ^ "."))
  |> ignore
- 
+
+/// automatic routing of control layer
+/// will try to connect each unconnected control line to some unconnected control punch
+/// according to the user settings
+[<CommandMethod("ConnectValvesToPunches")>]
+let connectValvesToPunches() =
+    let chip = Chip.create (Database.collectChipEntities())
+    let nLines = chip.ControlLayer.Lines.Length
+    let nUnconnectedLines = chip.ControlLayer.UnconnectedLines.Length
+    let nPunches = chip.ControlLayer.Punches.Length
+    let nUnconnectedPunches = chip.ControlLayer.UnconnectedPunches.Length
+    let outOf (nUnconnected : int) (n : int) =
+        if nUnconnected=n then "" else " (out of " ^ n.ToString() ^ ")"
+    Editor.writeLine (sprintf "Collected %d unconnected control lines%s and %d unconnected punches%s."
+                              nUnconnectedLines
+                              (outOf nUnconnectedLines nLines)
+                              nUnconnectedPunches
+                              (outOf nUnconnectedPunches nPunches))
+    if nUnconnectedLines = 0
+    then Editor.writeLine "Routing aborted, because there is nothing to connect."
+    else
+    if nUnconnectedPunches < nUnconnectedLines
+    then Editor.writeLine "Routing aborted, because the number of unconnected punches is less than the number of unconnected control lines."
+    else
+    let chipGrid =  Routing.createChipGrid chip    
+    let mcfSolution = Routing.minCostFlowRouting chipGrid
+    match mcfSolution with
+    | None -> 
+        Editor.writeLine "Routing failed: try more relaxed settings, perhaps."
+    | Some mcfSolution -> 
+        let iterativeSolver = new Routing.IterativeRouting (chipGrid, mcfSolution)
+        let presenter = Routing.presentConnections chipGrid
+        let n = iterativeSolver.stabilize()
+        let solution = iterativeSolver.Solution
+        solution |> presenter |> Database.writeEntities |> ignore
+        Editor.writeLine ("Routing succeeded: " ^ solution.Length.ToString() ^ " new connections.")
