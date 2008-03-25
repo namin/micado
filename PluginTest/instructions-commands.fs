9e8634d0a4afc475cefbe8c5a0a5a8969d8a8f4b
Nada AMIN <Nada AMIN@b571c760-3347-0410-a02b-4f11275fb890>
2008-03-25 06:25:27 +0000

    implemented instructions

--- PluginTest/instructions-commands.fs
+++ PluginTest/instructions-commands.fs
@@ -3,6 +3,7 @@
 module BioStream.Micado.Plugin.Test.Instructions.Commands
 
 open Autodesk.AutoCAD.Runtime
+open Autodesk.AutoCAD.DatabaseServices
 
 open BioStream.Micado.Common
 open BioStream.Micado.Core
@@ -19,6 +20,8 @@ let doc2ichip = new Dictionary<Autodesk.AutoCAD.ApplicationServices.Document, In
 
 let doc2namedFlowBoxes = new Dictionary<Autodesk.AutoCAD.ApplicationServices.Document, Map<string, Instructions.FlowBox.FlowBox>>()
 
+let doc2entity2instruction = new Dictionary<Autodesk.AutoCAD.ApplicationServices.Document, Dictionary<Entity, Instructions.Instruction>>()
+
 let defaultGet d (dict : Dictionary<'a,'b>) thunk =
     if not (dict.ContainsKey d)
     then dict.[d] <- thunk()
@@ -31,7 +34,12 @@ let getIChip() =
 
 let getNamedFlowBoxes() = 
     defaultGet (doc()) doc2namedFlowBoxes (fun () -> Map.empty)
-    
+
+let getEntity2Instruction() =
+    defaultGet (doc())
+               doc2entity2instruction
+               (fun () -> new Dictionary<Entity, Instructions.Instruction>())
+                   
 let setNamedFlowBoxes map =
     let d = doc()
     doc2namedFlowBoxes.[d] <- map
@@ -65,8 +73,8 @@ let getNamedFlowBoxesList() =
             let box = kv.Value
             yield ((prettyStringOfBox box) ^ " " ^ name), name]
     List.sort compare lst
-    
-let promptSelectBox message =
+
+let promptSelectBoxName message =
     let map = getNamedFlowBoxes()
     let lst = getNamedFlowBoxesList()
     let keys = lst |> List.map snd
@@ -77,9 +85,13 @@ let promptSelectBox message =
     Editor.promptSelectIdName message keys
  |> Option.bind (fun (s) -> 
                     if map.ContainsKey s
-                    then Some map.[s]
+                    then Some (map.[s], s)
                     else Editor.writeLine "no such box name"
                          None)
+    
+let promptSelectBox message =
+    promptSelectBoxName message
+ |> Option.map fst
 
 let promptBox f =
     let ic = getIChip()
@@ -101,7 +113,7 @@ let instruction_prompt_output_box() =
     promptBox Instructions.Interactive.promptOutputBox
 
 let promptSelectBoxes() =
-    let promptBox (i : int) = promptSelectBox ("Box name #" ^ i.ToString())
+    let promptBox (i : int) = promptSelectBox ("Box #" ^ i.ToString())
     let ra = new ResizeArray<Instructions.FlowBox.FlowBox>()
     let res1 = promptBox 1
     match res1 with
@@ -152,10 +164,44 @@ let instruction_clear_cache() =
     let d = doc()
     doc2ichip.Remove(d) |> ignore
     doc2namedFlowBoxes.Remove(d) |> ignore
+    doc2entity2instruction.Remove(d) |> ignore
     
 [<CommandMethod("micado_instruction_draw_box")>]    
-/// gets and draw an instruction
-let instruction_get() =
-    promptSelectBox "Box name"
+/// gets and draw a flow box
+let instruction_draw_box() =
+    promptSelectBox "Box "
  |> Option.map (fun (s) -> Debug.drawFlowBox (getIChip()) s)
+ |> ignore
+ 
+[<CommandMethod("micado_instruction_build")>]    
+/// build instructions from a flow box
+let instruction_build() =
+    let ic = getIChip()
+    match promptSelectBoxName "Box " with
+    | None -> ()
+    | Some (box, name) ->
+        Debug.drawFlowBox ic box
+        let instructions = Instructions.Interactive.promptInstructions ic name box
+        Editor.clearMarks()
+        match instructions with
+        | None -> ()
+        | Some instructions -> 
+            let entity2instruction = getEntity2Instruction()
+            for instruction in instructions do
+                entity2instruction.Add(instruction.Entity,instruction)
+
+[<CommandMethod("micado_instruction_see")>]    
+/// see an instruction
+let instruction_see() =
+    let seeInstruction entity =
+        let entity2instruction = getEntity2Instruction()
+        if not (entity2instruction.ContainsKey entity)
+        then Editor.writeLine "The selected entity is not associated with the extents of any instruction."
+        else
+        let instruction = entity2instruction.[entity]
+        Editor.writeLine ("instruction " ^ instruction.Name)
+        Debug.drawExtents (instruction.Extents)
+        Debug.drawUsed (getIChip()) (instruction.Used)
+    Editor.promptSelectEntity "Select the entity associated with the extents of the instruction: "
+ |> Option.map seeInstruction
  |> ignore
