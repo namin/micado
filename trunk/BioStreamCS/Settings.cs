2d63576791a638efe1935748dcfd502f8b7ee89a
Nada AMIN <Nada AMIN@b571c760-3347-0410-a02b-4f11275fb890>
2008-03-26 18:03:12 +0000

    Refactored Settings.cs so that the XML Serialization could be used with other types. Also made the Instruction type not dependent on InstructionChip (it was just a useless just-in-case dependence).

--- trunk/BioStreamCS/Settings.cs
+++ trunk/BioStreamCS/Settings.cs
@@ -17,68 +17,14 @@ namespace BioStream.Micado.User
 
         public static Settings Current = ImportCurrentSettings();
 
-        /// <summary>
-        /// To convert a Byte Array of Unicode values (UTF-8 encoded) to a complete String.
-        /// </summary>
-        /// <param name="characters">Unicode Byte Array to be converted to String</param>
-        /// <returns>String converted from Unicode Byte Array</returns>
-        private static String UTF8ByteArrayToString(Byte[] characters)
-        {
-            UTF8Encoding encoding = new UTF8Encoding();
-            String constructedString = encoding.GetString(characters);
-            return (constructedString);
-        }
-
-        /// <summary>
-        /// Converts the String to UTF8 Byte array and is used in De serialization
-        /// </summary>
-        /// <param name="pXmlString"></param>
-        /// <returns></returns>
-        private static Byte[] StringToUTF8ByteArray(String pXmlString)
-        {
-            UTF8Encoding encoding = new UTF8Encoding();
-            Byte[] byteArray = encoding.GetBytes(pXmlString);
-            return byteArray;
-        }
-
-        /// <summary>
-        /// Method to convert a custom Object to XML string
-        /// </summary>
-        /// <param name="pObject">Object that is to be serialized to XML</param>
-        /// <returns>XML string</returns>
         private static String SerializeObject(Object pObject)
         {
-            try
-            {
-                String XmlizedString = null;
-                MemoryStream memoryStream = new MemoryStream();
-                XmlSerializer xs = new XmlSerializer(typeof(Settings));
-                XmlTextWriter xmlTextWriter = new XmlTextWriter(memoryStream, Encoding.UTF8);
-
-                xs.Serialize(xmlTextWriter, pObject);
-                memoryStream = (MemoryStream)xmlTextWriter.BaseStream;
-                XmlizedString = UTF8ByteArrayToString(memoryStream.ToArray());
-                return XmlizedString;
-            }
-            catch (Exception e)
-            {
-                System.Console.WriteLine(e);
-                return null;
-            }
+            return BioStream.Micado.Common.Serialization.SerializeObject(typeof(Settings), pObject);
         }
 
-        /// <summary>
-        /// Method to reconstruct an Object from XML string
-        /// </summary>
-        /// <param name="pXmlizedString"></param>
-        /// <returns></returns>
         private static Object DeserializeObject(String pXmlizedString)
         {
-            XmlSerializer xs = new XmlSerializer(typeof(Settings));
-            MemoryStream memoryStream = new MemoryStream(StringToUTF8ByteArray(pXmlizedString));
-            XmlTextWriter xmlTextWriter = new XmlTextWriter(memoryStream, Encoding.UTF8);
-
-            return xs.Deserialize(memoryStream);
+            return BioStream.Micado.Common.Serialization.DeserializeObject(typeof(Settings), pXmlizedString);
         }
 
         internal static void ExportSettings(Settings settings, string filepath)
